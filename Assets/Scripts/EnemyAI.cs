6c5d6a8ce03a5bb65083ad3b6eff1c0bde5fd712
Gord Bolli <gord@bolli.ca>
2023-04-14 12:01:09 -0700

    First commit, basic sandbox, enemy with navMeshAgent and AI, player with FPS setup

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+
+    float chaseRange = 10f;
+    float distanceToTarget = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (distanceToTarget <= chaseRange) EngageTarget();
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) AttackTarget();
+        else ChaseTarget();
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("HaHaHa!   Got you!");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
