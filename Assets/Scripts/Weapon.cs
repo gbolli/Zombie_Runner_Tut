089e29154e90908ee4ec080ef4f1d375a3a26ef6
Gord Bolli <gord@bolli.ca>
2023-04-20 10:40:22 -0700

    Added weapon hit effect on other objects (sparks)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,7 +10,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 50f;
     [SerializeField] ParticleSystem muzzleFlash;
-    [SerializeField] GameObject hitEffect;
+    [SerializeField] GameObject fleshHitEffect;
+    [SerializeField] GameObject surfaceHitEffect;
 
     void Update()
     {
@@ -33,17 +34,26 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
+            if (hit.transform.name == "Enemy")
+            {
+                CreateHitEffect(hit, fleshHitEffect);
+            }
+            else
+            {
+                CreateHitEffect(hit, surfaceHitEffect);
+            }
+
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+
             if (target == null) return;
 
-            CreateHitEffect(hit);
             target.TakeDamage(damage);
-        };
+        }
     }
 
-    private void CreateHitEffect(RaycastHit hit)
+    private void CreateHitEffect(RaycastHit hit, GameObject effect)
     {
-        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        GameObject impact = Instantiate(effect, hit.point, Quaternion.LookRotation(hit.normal));
         Destroy(impact, 1);
     }
 }
