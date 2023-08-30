23d3b7f378766eb982558afa34b9410a807a7d97
Gord Bolli <gord@bolli.ca>
2023-08-30 13:14:03 -0700

    Create player flashlight with reducing illumination and angle as battery runs out

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 0.1f;
+    [SerializeField] float minimumAngle = 30f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        if (myLight.spotAngle > minimumAngle)
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightAngle()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
