bb3d106229b3a55cbe10294169a6861a50a0f2c9
Gord Bolli <gord@bolli.ca>
2023-09-03 16:42:45 -0700

    Created damage display canvas and show when taking damage.

---
+++ Assets/Scripts/DisplayDamage.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayDamage : MonoBehaviour
+{
+
+    [SerializeField] Canvas bloodSplatterCanvas;
+    [SerializeField] float displayTime = 0.3f;
+
+
+    void Start()
+    {
+        bloodSplatterCanvas.enabled = false;
+    }
+
+    public void ShowBloodSplatterCanvas()
+    {
+        StartCoroutine(ShowBloodSplatter());
+    }
+    
+    IEnumerator ShowBloodSplatter()
+    {
+        bloodSplatterCanvas.enabled = true;
+        yield return new WaitForSeconds(displayTime);
+        bloodSplatterCanvas.enabled = false;
+    }
+}
